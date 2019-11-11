f320b8d859310c170e55e47b14f7ba23ce1e46e0
stephane ardaens <ardaens.stephane@gmail.com>
2019-11-11 11:09:15 +0100

    feat(app) : add operation

---
+++ src/main/java/com/mycompany/app/MathService.java
@@ -0,0 +1,14 @@
+package com.mycompany.app;
+
+public class MathService {
+
+	
+	public int add(Integer i, Integer j)
+	{
+		if (i != null && j != null)
+			return i+j;
+		else
+			return 0;
+	}
+	
+}
